da78cae474fecf3bae0dc601b170ae663ad8f0f7
Quentin Gaborit <gibboneto@gmail.com>
2022-11-12 13:36:54 +0100

    chore: Remove pants 3rdparty locks experimentation

--- templates/pants-default-lock/utils/package_1/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def action_1():
-    print("Doing stuff from package 1")
-    return
--- templates/pants-default-lock/utils/package_2/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def action_2():
-    print("Doing stuff from package 2")
-    return
